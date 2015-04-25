122a81b9a478a875441de2f15dcf7f616ceb88e1
asingleton39 <amyts@bu.edu>
2015-04-25 16:16:23 -0400

    floors

--- app/src/main/java/com/example/amy/hydrate/frag_third_floor.java
+++ app/src/main/java/com/example/amy/hydrate/frag_third_floor.java
@@ -6,19 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class frag_third_floor extends Fragment
 {
 
-    ImageButton floor_1_1;
+    Button floor_3_1;
+    Button floor_3_2;
+    Button floor_3_3;
 
     @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View InputFragmentView = inflater.inflate(R.layout.fragfloor3, container, false);
-        floor_1_1 = (ImageButton) InputFragmentView.findViewById(R.id.f_1_1);
+        floor_3_1 = (Button) InputFragmentView.findViewById(R.id.f_3_1);
 
-        floor_1_1.setOnClickListener(new View.OnClickListener() {
+        floor_3_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent resultActivity = new Intent(getActivity(), rating.class);
@@ -26,6 +29,30 @@ public class frag_third_floor extends Fragment
 
             }
         });
+
+        floor_3_2 = (Button) InputFragmentView.findViewById(R.id.f_3_2);
+
+        floor_3_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultActivity = new Intent(getActivity(), rating.class);
+                getActivity().startActivity(resultActivity);
+
+            }
+        });
+
+        floor_3_3 = (Button) InputFragmentView.findViewById(R.id.f_3_3);
+
+        floor_3_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultActivity = new Intent(getActivity(), rating.class);
+                getActivity().startActivity(resultActivity);
+
+            }
+        });
+
+
         return InputFragmentView;
     }
 }
