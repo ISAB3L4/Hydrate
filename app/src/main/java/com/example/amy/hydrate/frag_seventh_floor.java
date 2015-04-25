48c2ce5113fb04cb752622cdfb88923a89567068
jzurita <jeffrey_zurita@jzurita.com>
2015-04-25 12:15:24 -0400

    more buttons

--- app/src/main/java/com/example/amy/hydrate/frag_seventh_floor.java
+++ app/src/main/java/com/example/amy/hydrate/frag_seventh_floor.java
@@ -1,18 +1,31 @@
 package com.example.amy.hydrate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
-public class frag_seventh_floor extends Fragment {
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
-        return inflater.inflate(R.layout.fragfloor7,container,false);
+public class frag_seventh_floor extends Fragment
+{
+
+    ImageButton floor_1_1;
+
+    @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View InputFragmentView = inflater.inflate(R.layout.fragfloor7, container, false);
+        floor_1_1 = (ImageButton) InputFragmentView.findViewById(R.id.f_1_1);
+
+        floor_1_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultActivity = new Intent(getActivity(), rating.class);
+                getActivity().startActivity(resultActivity);
+
+            }
+        });
+        return InputFragmentView;
     }
-
-
 }
