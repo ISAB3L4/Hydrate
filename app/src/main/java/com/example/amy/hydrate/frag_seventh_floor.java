f3237b32e9a79e5f416d3eff37e567dcfce2aea1
asingleton39 <amyts@bu.edu>
2015-04-25 16:13:08 -0400

    floors

--- app/src/main/java/com/example/amy/hydrate/frag_seventh_floor.java
+++ app/src/main/java/com/example/amy/hydrate/frag_seventh_floor.java
@@ -6,19 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class frag_seventh_floor extends Fragment
 {
 
-    ImageButton floor_1_1;
+    Button floor_7_1;
+    Button floor_7_2;
+    Button floor_7_2;
 
     @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View InputFragmentView = inflater.inflate(R.layout.fragfloor7, container, false);
-        floor_1_1 = (ImageButton) InputFragmentView.findViewById(R.id.f_1_1);
+        floor_7_1 = (ImageButton) InputFragmentView.findViewById(R.id.f_7_1);
 
-        floor_1_1.setOnClickListener(new View.OnClickListener() {
+        floor_7_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent resultActivity = new Intent(getActivity(), rating.class);
@@ -26,6 +29,32 @@ public class frag_seventh_floor extends Fragment
 
             }
         });
+
+        floor_7_2 = (ImageButton) InputFragmentView.findViewById(R.id.f_7_2);
+
+        floor_7_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultActivity = new Intent(getActivity(), rating.class);
+                getActivity().startActivity(resultActivity);
+
+            }
+        });
+
+        floor_7_3 = (ImageButton) InputFragmentView.findViewById(R.id.f_7_3);
+
+        floor_7_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultActivity = new Intent(getActivity(), rating.class);
+                getActivity().startActivity(resultActivity);
+
+            }
+        });
+
+
+
+
         return InputFragmentView;
     }
 }
