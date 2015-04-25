7e44159d6e4c875ac4c65547b31a3455bd4c1e24
jzurita <jeffrey_zurita@jzurita.com>
2015-04-25 11:58:03 -0400

    fixed this blshit

--- app/src/main/java/com/example/amy/hydrate/PageAdapter.java
+++ app/src/main/java/com/example/amy/hydrate/PageAdapter.java
@@ -10,6 +10,7 @@ class PagerAdapter extends FragmentPagerAdapter {
         super(fm);
         // TODO Auto-generated constructor stub
     }
+
     @Override
     public Fragment getItem(int arg0) {
         // TODO Auto-generated method stub
@@ -17,22 +18,20 @@ class PagerAdapter extends FragmentPagerAdapter {
             case 0:
                 return new frag_basement_floor();
             case 1:
-                return new frag_first_floor();
-            case 2:
                 return new frag_second_floor();
-            case 3:
+            case 2:
                 return new frag_third_floor();
-            case 4:
+            case 3:
                 return new frag_fourth_floor();
-            case 5:
+            case 4:
                 return new frag_fifth_floor();
-            case 6:
+            case 5:
                 return new frag_sixth_floor();
-            case 7:
+            case 6:
                 return new frag_seventh_floor();
-            case 8:
+            case 7:
                 return new frag_eighth_floor();
-            case 9:
+            case 8:
                 return new frag_ninth_floor();
             default:
                 break;
@@ -43,7 +42,7 @@ class PagerAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         // TODO Auto-generated method stub
-        return 10   ;
+        return 9   ;
     }
 
 
