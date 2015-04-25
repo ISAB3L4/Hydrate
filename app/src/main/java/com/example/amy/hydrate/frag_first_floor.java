c7caa29ce6d001386e88cb5feb1889b68c29bbc9
jzurita <jeffrey_zurita@jzurita.com>
2015-04-25 12:15:09 -0400

    got button to work!

---
+++ app/src/main/java/com/example/amy/hydrate/frag_first_floor.java
@@ -0,0 +1,31 @@
+package com.example.amy.hydrate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+public class frag_first_floor extends Fragment
+    {
+
+        ImageButton floor_1_1;
+
+        @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View InputFragmentView = inflater.inflate(R.layout.floorfrag1, container, false);
+            floor_1_1 = (ImageButton) InputFragmentView.findViewById(R.id.f_1_1);
+
+            floor_1_1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent resultActivity = new Intent(getActivity(), rating.class);
+                    getActivity().startActivity(resultActivity);
+
+                }
+            });
+            return InputFragmentView;
+        }
+    }
