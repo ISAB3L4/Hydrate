ce032626a05561e7cd9586165119c19bf13fce28
asingleton39 <amyts@bu.edu>
2015-04-25 14:13:11 -0400

    basement buttons

--- app/src/main/java/com/example/amy/hydrate/frag_basement_floor.java
+++ app/src/main/java/com/example/amy/hydrate/frag_basement_floor.java
@@ -6,19 +6,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
+import android.widget.Button;
 
 public class frag_basement_floor extends Fragment
 {
 
-    ImageButton floor_1_1;
+    Button floor_b_1;
+    Button floor_b_2;
 
     @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View InputFragmentView = inflater.inflate(R.layout.floorfragb, container, false);
-        floor_1_1 = (ImageButton) InputFragmentView.findViewById(R.id.f_1_1);
+        floor_b_1 = (Button) InputFragmentView.findViewById(R.id.f_b_1);
+        floor_b_2 = (Button) InputFragmentView.findViewById(R.id.f_b_2);
 
-        floor_1_1.setOnClickListener(new View.OnClickListener() {
+        floor_b_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultActivity = new Intent(getActivity(), rating.class);
+                getActivity().startActivity(resultActivity);
+
+            }
+        });
+
+        floor_b_2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent resultActivity = new Intent(getActivity(), rating.class);
